380137aecb510432089c047a901fb174f58e2e55
Alexis López Zubieta <azubieta90@gmail.com>
2017-09-18 18:15:58 -0500

    Creating nomad panel layout from code.

--- look-and-feel/org.kde.nomad.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ look-and-feel/org.kde.nomad.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,61 +1,29 @@
-var plasma = getApiVersion(1);
+var plasma = getApiVersion(1)
 
-var layout = {
-    "desktops": [
-        {
-            "applets": [
-            ],
-            "config": {
-                "/": {
-                    "formfactor": "0",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/ConfigDialog": {
-                    "DialogHeight": "480",
-                    "DialogWidth": "640"
-                },
-                "/General": {
-                    "ToolBoxButtonState": "bottomright",
-                    "ToolBoxButtonX": "2544",
-                    "ToolBoxButtonY": "997",
-                    "showToolbox": "false"
-                },
-                "/Wallpaper/org.kde.image/General": {
-                    "FillMode": "2",
-                    "Image": "file:///home/alexis/Pictures/BdNvWmA.png"
-                }
-            },
-            "wallpaperPlugin": "org.kde.image"
-        }
-    ],
-    "panels": [
-        {
-            "alignment": "left",
-            "applets": [],
-            "config": {
-                "/": {
-                    "formfactor": "2",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                },
-                "/ConfigDialog": {
-                    "DialogHeight": "80",
-                    "DialogWidth": "2560"
-                }
-            },
-            "height": 1.875,
-            "hiding": "normal",
-            "location": "top",
-            "maximumLength": 160,
-            "minimumLength": 160,
-            "offset": 0
-        },
-    ],
-    "serializationFormatVersion": "1"
-}
-;
+// Create top pannel
+panel = new Panel()
+panel.location = "top"
+panel.alignment = "center"
 
-plasma.loadSerializedLayout(layout);
+// Add widgets
+var menu = panel.addWidget("org.kde.plasma.nomadmenu")
+
+// Configure favorite apps
+menu.currentConfigGroup = ["General"]
+menu.writeConfig("favoriteApps", "systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,chromium-browser.desktop")
+    // Configure menu shortcut
+menu.currentConfigGroup = ["Shortcuts"]
+menu.writeConfig("global", "Alt+F1")
+
+
+panel.addWidget("org.kde.plasma.appmenu")
+panel.addWidget("org.kde.plasma.panelspacer")
+var systray = panel.addWidget("org.nomad.systemtray")
+
+// Don't load widgets overwriten in nomad
+var systrayContainmentId = systray.readConfig('SystrayContainmentId')
+var systrayContainer = desktopById(systrayContainmentId)
+systrayContainer.currentConfigGroup = ['General']
+systrayContainer.writeConfig("knownItems", "org.kde.plasma.networkmanagement,org.kde.plasma.notifications,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.volume")
+
+panel.addWidget("org.nomad.clock")
