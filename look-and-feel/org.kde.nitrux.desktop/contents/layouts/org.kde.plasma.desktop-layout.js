f6a35b19a39834483a9846230e7478c070d29a9a
Uri Herrera <hello@uriherrera.com>
2020-06-20 03:50:54 -0500

    update pkg

--- look-and-feel/org.kde.nitrux.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ look-and-feel/org.kde.nitrux.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -8,7 +8,7 @@ activity.writeConfig("ToolBoxButtonState", "bottomright")
 activity.writeConfig("showToolbox", false)
 
 activity.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General')
-activity.writeConfig('Image', 'Opal')
+activity.writeConfig('Image', 'Opal Colors')
 
 // // Create top pannel
 // panel = new plasma.Panel
